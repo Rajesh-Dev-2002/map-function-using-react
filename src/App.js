a709e8cbfd6797a19971d29b0633d136fa44d88a
Rajesh kumar Mohanta <rajeshkumarmohanta2001@gmail.com>
2023-10-15 22:34:37 +0530

    hell git

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { restaurant } from './restaurant'
 
 const App = () => {
-  const [change,setChange]= useState("Hello")
-  // const change = "hello"
   return (
-    
-    <>
     <div>
-      <input type="text" placeholder='search text' onChange={(e)=>setChange(e.target.value)}  value={change}/>
-      {/* // because it is a data one way data binding binding */}
-      <button>Submit--{change}</button>
-      
+      {restaurant.map((item)=>(
+        <img src={item.url} alt="" />
+      ))}
     </div>
-    </>
   )
 }
 
---
+++ src/restaurant.js
@@ -0,0 +1,30 @@
+export const restaurant = [
+    {
+        name:"Coffee Nation",
+        url:"https://b.zmtcdn.com/data/pictures/2/19459772/e529830f9029980262383177791d485d_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+        name:"Burger - Since 1989",
+        url:"https://b.zmtcdn.com/data/pictures/chains/5/10215/8290f2b307232f45a7ee2b8b7c3ae1e5_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+        name:"Anna",
+        url:"https://b.zmtcdn.com/data/pictures/chains/3/18502363/0a9b1da05f356b20463b6cebf71b2f14_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+        name:"Imperial Bakery - Since 1955",
+        url:"https://b.zmtcdn.com/data/pictures/3/6506233/c5f1cff2eb3a953e6982578fa4f675b0_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+        name:"MH 12 Pav Bhaji",
+        url:"https://b.zmtcdn.com/data/pictures/0/13480/30c5d30c1ff20a64c778cedf075344ba_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+        name:"Chitale Bandhu Mithaiwale",
+        url:"https://b.zmtcdn.com/data/pictures/chains/9/13279/21f5059f5c291659fb5ea3e1a75ea0f1_o2_featured_v2.jpg?output-format=webp"
+    },
+    {
+        name:"Dilkhush Chinese Fast Food",
+        url:"https://b.zmtcdn.com/data/pictures/4/11344/3dab8a0bf55dab1db9c1ea310bca45b9_o2_featured_v2.jpg"
+    },
+]
